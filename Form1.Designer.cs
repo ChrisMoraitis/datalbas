0be08925d585965799883485f78a85149ba95d19
ChrisMoraitis <47454972+ChrisMoraitis@users.noreply.github.com>
2019-05-12 19:25:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TTS_Eng_text
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.richTextBoxTTS = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(275, 168);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(235, 66);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "playNlisten";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // richTextBoxTTS
+            // 
+            this.richTextBoxTTS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxTTS.Location = new System.Drawing.Point(49, 13);
+            this.richTextBoxTTS.Name = "richTextBoxTTS";
+            this.richTextBoxTTS.Size = new System.Drawing.Size(705, 98);
+            this.richTextBoxTTS.TabIndex = 1;
+            this.richTextBoxTTS.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxTTS);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.RichTextBox richTextBoxTTS;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+
+namespace TTS_Eng_text
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //make speech synthesis
+        SpeechSynthesizer speaker = new SpeechSynthesizer();
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            if (richTextBoxTTS.Text != "")
+            {
+                speaker.Dispose();
+                speaker = new SpeechSynthesizer();
+                speaker.SpeakAsync(richTextBoxTTS.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a text first!");
+            }
+        }
+    }
+}
